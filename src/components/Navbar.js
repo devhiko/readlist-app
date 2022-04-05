0f3af2fd58f110f2b8492a589efe2e5708335280
devhiko <aydin96tr@gmail.com>
2022-04-05 14:54:36 +0300

    fix code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ class Navbar extends Component {
     return (
       <ThemeContext.Consumer>
         {context => {
-          const { isLightTheme, light, dark } = this.context;
+          const { isLightTheme, light, dark } = context;
           const theme = isLightTheme ? light : dark;
           return (
-            <nav style={{ background: theme.bg, color: theme.syntax }}>
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
               <h1>Context App</h1>
               <ul>
                 <li>Home</li>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
   state = {
-    isLightTheme: false,
+    isLightTheme: true,
     light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
     dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
   }
