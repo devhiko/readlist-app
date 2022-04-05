3f69e747cdb2b4558a56819d93e141c6c1720f5e
devhiko <aydin96tr@gmail.com>
2022-04-05 19:01:47 +0300

    come to functional context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 // this way is more useful and wise.
-class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>{authContext => (
-        <ThemeContext.Consumer>{themeContext => {
-          const { isAuth, toggleAuth } = authContext;
-          const { isLightTheme, light, dark } = themeContext;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav
-              style={{ background: theme.ui, color: theme.syntax }}
-            >
-              <h1>Context App</h1>
-              <div onClick={toggleAuth}>
-                {isAuth ? 'Logged In' : 'Logged Out'}
-              </div>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          )
-        }}
-          {/* {ctx=>{
-          ...
-          return(JSX)
-      }} */}
-        </ThemeContext.Consumer>
-      )}
-      </AuthContext.Consumer>
-    );
-  }
+const Navbar = () => {
+  const { isAuth, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav
+      style={{ background: theme.ui, color: theme.syntax }}
+    >
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuth ? 'Logged In' : 'Logged Out'}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ src/contexts/BookContext.js
@@ -0,0 +1,11 @@
+import React, { createContext, useState } from 'react';
+
+export const BookContext = createContext();
+
+const BookContext = (props) => {
+  return (
+    <div>test</div>
+  );
+}
+
+export default BookContext;
